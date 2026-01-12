9ebfb1d04b63e062c6ac43f1f2ddbfc4c09df484
krishnamoorthyBFX <krishnamoorthy.k@bfxinfotech.com>
2026-01-12 21:56:08 +0530

    feat: create app and setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl">
+        Namaste React Lets Build Netflix gpt
+      </h1>
+    </>
+  );
+}
+
+export default App;
